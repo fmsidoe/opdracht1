6829756758533fb2ec6c6e6a6ab2d77bd783d6ab
Fazal Idoe <Fazal@.desizone.nl>
2010-10-25 11:55:19 +0200

    1st gitupload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opdracht1::Application.config.secret_token = '598701354c7bb6818bd5486430d7ae941d285484ab4e3b721e083e38f3ab9ad121768f08ba096d73572dc0dab2e6bf6a8298c091e77a2de52f7c337d51e4358a'
